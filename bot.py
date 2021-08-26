68571574197f055b43f78ec53ae3b8547e95498e
Hasalanka Seneviratne <seneviratnehasalanka@gmail.com>
2021-08-27 01:41:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,12 @@ from pyrogram import Client
 
 app = Client(
     "my_bot",
-    bot_token="1664618022:AAF535rU3lB4bq97MhHffLVzqHhJ_lq3bzU"
+    bot_token=config.BOT_TOKEN,
 )
+
+async def main():
+    async with app:
+        await app.send_message("me", "Hi!")
+
 
 app.run()
