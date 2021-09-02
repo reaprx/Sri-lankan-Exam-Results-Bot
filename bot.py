459d0540f80ba3ef670f4ad7389773441028257e
Hasalanka <seneviratnehasalanka@gmail.com>
2021-09-02 23:54:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot("1664618022:AAF535rU3lB4bq97MhHffLVzqHhJ_lq3bzU")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Hello I'm a telegram bot create by @reaprx")
+	bot.reply_to(message, "Hello I'm a telegram bot created by @reaprx")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
