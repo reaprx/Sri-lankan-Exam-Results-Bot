dc7abf83c53bbb49fcce3693855dc34d6ade244d
Hasalanka <seneviratnehasalanka@gmail.com>
2021-09-02 23:10:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
-from pyrogram import Client
+import telebot
 
-app = Client(
-    "my_bot",
-    bot_token="1664618022:AAF535rU3lB4bq97MhHffLVzqHhJ_lq3bzU"
-)
+bot = telebot.TeleBot("1664618022:AAF535rU3lB4bq97MhHffLVzqHhJ_lq3bzU")
 
-app.send_message("me", "Hi!")
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Hello I'm a telegram bot create by @reaprx")
 
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
 
-app.run()
+bot.polling()
