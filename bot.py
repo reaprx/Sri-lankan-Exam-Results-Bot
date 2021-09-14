3f8d9b30514d4fede2a59a7428f4282ca93c7975
Hasalanka <seneviratnehasalanka@gmail.com>
2021-09-15 03:20:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,79 @@
+import os
 import telebot
-import webbrowser
+import re
+import requests
+import json
 
-webbrowser.open("https:/zoom.us")
+bot_token = "1664618022:AAHCrrmXi4yGfBdyViHonBUnBXpvmxIA2lE"
 
-bot = telebot.TeleBot("1664618022:AAEG_LYU49USJpklBe9r4f5T_xhDu2s-F8g")
+bot = telebot.TeleBot(bot_token)
 
-@bot.message_handler(commands=['start', 'help'])
+def alresult(inumber):
+  result_json = requests.get("https://www.doenets.lk/result/service/AlResult/"+inumber)
+  try:
+   re = json.loads(result_json.text)
+   global res
+   res = "\nExam - " + re['examination'] + "\nYear - "+ re['year'] + "\nName - " + re['name'] + "\nIndex Number - " + re['indexNo'] + "\nNIC Number - " + re['nic'] + "\nDistrict Rank - " + re['districtRank'] + "\nIsland Rank - " + re['islandRank'] + "\nZScore - " + re['zScore'] + "\nSubject Stream - " + re['stream'] + "\nResults \n" + re['subjectResults'][0]['subjectName'] + " - " + re['subjectResults'][0]['subjectResult'] + "\n" + re['subjectResults'][1]['subjectName'] + " - " + re['subjectResults'][1]['subjectResult'] + "\n" + re['subjectResults'][2]['subjectName'] + " - " + re['subjectResults'][2]['subjectResult'] + "\n" + re['subjectResults'][3]['subjectName'] + " - " + re['subjectResults'][3]['subjectResult'] + "\n" + re['subjectResults'][4]['subjectName'] + " - " + re['subjectResults'][4]['subjectResult']
+  except TypeError:
+   res="Wrong Index Number"
+
+def olresult(inumber):
+  result_json = requests.get("https://www.doenets.lk/result/service/OlResult/"+inumber)
+  try:  
+    re = json.loads(result_json.text)
+    global res
+    res = "\nExam - " + re['examination'] +"\nYear - " + re['year'] + "\nName - " + re['name'] + "\nIndex Number - " + re['indexNo'] + "\nResults \n" + re['subjectResults'][0]['subjectName'] + " - " + re['subjectResults'][0]['subjectResult'] + "\n" + re['subjectResults'][1]['subjectName'] + " - " + re['subjectResults'][1]['subjectResult'] + "\n" + re['subjectResults'][2]['subjectName'] + " - " + re['subjectResults'][2]['subjectResult'] + "\n" + re['subjectResults'][3]['subjectName'] + " - " + re['subjectResults'][3]['subjectResult'] + "\n" + re['subjectResults'][4]['subjectName'] + " - " + re['subjectResults'][4]['subjectResult'] + "\n" + re['subjectResults'][5]['subjectName'] + " - " + re['subjectResults'][5]['subjectResult'] + "\n" + re['subjectResults'][6]['subjectName'] + " - " + re['subjectResults'][6]['subjectResult'] + "\n" + re['subjectResults'][7]['subjectName'] + " - " + re['subjectResults'][7]['subjectResult'] + "\n" + re['subjectResults'][8]['subjectName'] + " - " + re['subjectResults'][8]['subjectResult']
+  except TypeError:
+    res="Wrong Index Number"
+
+
+def g5result(inumber):
+  result_json = requests.get("https://www.doenets.lk/result/service/GvResult/"+inumber)
+  try:  
+    re = json.loads(result_json.text)
+    global res
+    res = "\nExam - " + re['examination'] + "\nYear - " + re['year'] + "\nName - " + re['name'] + "\nIndex Number - " + re['indexNo'] + "\nMarks - " + re['marks'] + "\nDistrict Rank - " + re['districtRank']
+  except TypeError:
+    res="Wrong Index Number"
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Hello I'm a telegram bot created by @reaprx")
+	bot.reply_to(message, "Hello I can find exam results.\n/help for more info\n\nPowered By @xreapr")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+@bot.message_handler(commands=['help'])
+def send_help(message):
+	bot.reply_to(message, """To get Results send Index Number as Following.
+
+/al {Index nNumber}  -  To get AL Rsults
+/ol {Index nNumber}  -  To get OL Rsults
+/g5 {Index nNumber}  -  To get Grade 5 Scholarship Exam Rsults""")
+
+@bot.message_handler(commands=['al','AL','Al'])
+def send_al(message):
+  x = re.split("\s", message.text)
+  try:
+    alresult(x[1])
+    bot.reply_to(message,res)
+  except:
+    bot.reply_to(message, "Index Number?")
+  
+@bot.message_handler(commands=['ol','OL','Ol'])
+def send_ol(message):
+   x = re.split("\s", message.text)
+   try:
+     olresult(x[1])
+     bot.reply_to(message, res)
+   except:
+     bot.reply_to(message, "Index Number?")
+     
+
+@bot.message_handler(commands=['g5','G5'])
+def send_g5(message):
+   x = re.split("\s", message.text)
+   try:
+     g5result(x[1])
+     bot.reply_to(message, res)
+   except:
+     bot.reply_to(message, "Index Number?")
 
 bot.polling()
